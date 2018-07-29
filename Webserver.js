6651db52c25f2ec4668cefb2fc38cd78677c935a
KJH <cogito1016@naver.com>
2018-07-29 22:27:09 +0900

    Reunderstanding Web application code (Anonymous function & parameter & module & port & IP ... )

--- Webserver.js
+++ Webserver.js
@@ -1,13 +1,19 @@
-
-
-
+//Loading Http module.
 const http = require('http');
 
 const hostname = '127.0.0.1';
 const port = 1337;
 
+//Using Method createServer in http module
+//Structre//
+var server = http.createServer();
+server.listen();
 
-http.createServer((req, res) => { res.writeHead(200, { 'Content-Type': 'text/plain' });
+// Plus Anonymous function & method & parameter (port,hostname, callbackFNC) ...
+
+//Full code
+http.createServer((req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
     res.end('Hello World\n');
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
