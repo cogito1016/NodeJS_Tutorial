8eb2e4c86b10483f5e3cdf61d25e0c5c72297265
KJH <cogito1016@naver.com>
2018-07-31 00:02:59 +0900

    Test using htmlFile & PNG to nodejs

--- expressTest.js
+++ expressTest.js
@@ -6,10 +6,14 @@ var express = require('express');
 //return application using call 'express' method
 var app = express();
 
+//Add this sentence 'use method'
+app.use(express.static('public'));
+
 //If user connect home, run function(req,res)...
 app.get('/',function(req,res){
     res.send('hello home page');
 });
+
 
 app.get('/login',function(req,res){
     res.send('Login please');
